a9507a06d00ae92da58a29c41ad7424bd940ec01
Evgeniy Vasilyev <evgeniy.vaslv@gmail.com>
2026-09-25 23:10:14 +0300

    feat: implement primitive event-update-render loop

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,53 @@
+use std::ffi::c_void;
+use std::thread;
+use std::time::Duration;
+
 unsafe extern "C" {
     fn smgn_sdl_init();
     fn smgn_sdl_quit();
+    fn smgn_sdl_get_window() -> *const c_void;
+    fn smgn_sdl_get_renderer(sdl_window: *const c_void) -> *const c_void;
+    fn smgn_sdl_nuke_window(sdl_window: *const c_void);
+    fn smgn_sdl_nuke_renderer(sdl_renderer: *const c_void);
+    fn smgn_sdl_render(sdl_renderer: *const c_void, r: u8, g: u8, b: u8);
+    fn smgn_init() -> *const c_void;
+    fn smgn_quit(smgn: *const c_void);
+    fn smgn_process_events(smgn: *const c_void);
+    fn smgn_get_should_quit(smgn: *const c_void) -> bool;
 }
 
 fn main() {
     unsafe { smgn_sdl_init() };
+
+    let window_ptr = unsafe { smgn_sdl_get_window() };
+    let renderer_ptr = unsafe { smgn_sdl_get_renderer(window_ptr) };
+    let smgn_ptr = unsafe { smgn_init() };
+
+    let mut r: u8 = 67;
+    let mut g: u8 = 128;
+    let mut b: u8 = 200;
+
+    loop {
+        unsafe { smgn_process_events(smgn_ptr) };
+
+        let should_quit = unsafe { smgn_get_should_quit(smgn_ptr) };
+
+        if should_quit {
+            break;
+        }
+
+        r = r.wrapping_add(1);
+        g = g.wrapping_add(1);
+        b = b.wrapping_add(1);
+
+        unsafe { smgn_sdl_render(renderer_ptr, r, g, b) };
+
+        let sleep_duration = Duration::from_millis(16);
+        thread::sleep(sleep_duration);
+    }
+
+    unsafe { smgn_sdl_nuke_renderer(renderer_ptr) };
+    unsafe { smgn_sdl_nuke_window(window_ptr) };
+    unsafe { smgn_quit(smgn_ptr) };
     unsafe { smgn_sdl_quit() };
 }
--- src/sdl3.c
+++ src/sdl3.c
@@ -1,11 +1,76 @@
+#include <stdlib.h>
 #include <SDL3/SDL.h>
 
 void smgn_sdl_init() {
-    SDL_InitFlags sdl_init_flags = 0;
+    SDL_InitFlags sdl_init_flags = SDL_INIT_VIDEO;
 
     SDL_Init(sdl_init_flags);
 }
 
 void smgn_sdl_quit() {
     SDL_Quit();
+}
+
+typedef struct
+{
+    bool should_quit;
+} smgn_t;
+
+smgn_t* smgn_init() {
+    return calloc(1, sizeof(smgn_t));
+}
+
+void smgn_quit(smgn_t* smgn) {
+    free(smgn);
+}
+
+void smgn_process_events(smgn_t* smgn) {
+    SDL_Event sdl_event;
+    while(SDL_PollEvent(&sdl_event)) {
+        switch (sdl_event.type) {
+        case SDL_EVENT_QUIT:
+            smgn->should_quit = true;
+            break;
+        }
+    }
+}
+
+bool smgn_get_should_quit(smgn_t* smgn) {
+    return smgn->should_quit;
+}
+
+SDL_Window* smgn_sdl_get_window() {
+    SDL_WindowFlags sdl_window_flags = 0;
+
+    SDL_Window* window = SDL_CreateWindow("Window test", 900, 450, sdl_window_flags);
+
+    if (!window) {
+        SDL_Log("Failed to create window: %s", SDL_GetError());
+    }
+
+    return window;
+}
+
+void smgn_sdl_nuke_window(SDL_Window* window) {
+    SDL_DestroyWindow(window);
+}
+
+SDL_Renderer* smgn_sdl_get_renderer(SDL_Window* window) {
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, NULL);
+
+    if (!renderer) {
+        SDL_Log("Failed to create renderer for window: %s", SDL_GetError());
+    }
+
+    return renderer;
+}
+
+void smgn_sdl_nuke_renderer(SDL_Renderer* renderer) {
+    SDL_DestroyRenderer(renderer);
+}
+
+void smgn_sdl_render(SDL_Renderer* renderer, uint8_t r,uint8_t g,uint8_t b){
+    SDL_SetRenderDrawColor(renderer, r,g,b,SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
 }
