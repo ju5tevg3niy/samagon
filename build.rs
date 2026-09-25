a256a780b86bb9443170c8116cdc14a05e477359
Evgeniy Vasilyev <evgeniy.vaslv@gmail.com>
2026-09-25 20:38:42 +0300

    feat: switch between debug/release meson build in build.rs

--- build.rs
+++ build.rs
@@ -13,18 +13,29 @@ where
 }
 
 fn setup_ffi_linking<P: AsRef<Path>>(out_dir: P) -> anyhow::Result<()> {
+    let debug = read_env("DEBUG")?;
+
+    let buildtype = match debug.as_str() {
+        "true" => "debug",
+        "false" => "release",
+        _ => anyhow::bail!("DEBUG env var contains {debug}"),
+    };
+
     let meson_build_dir = out_dir.as_ref().join("meson-build");
 
     let setup_status = Command::new("meson")
         .arg("setup")
         .arg("--reconfigure")
         .arg(meson_build_dir.as_os_str())
+        .arg("--buildtype")
+        .arg(buildtype)
         .status()
         .context("Failed to setup meson build directory")?;
 
     assert!(setup_status.success());
 
     let compile_status = Command::new("ninja")
+        .arg("--verbose")
         .arg("-C")
         .arg(meson_build_dir.as_os_str())
         .status()
@@ -33,16 +44,16 @@ fn setup_ffi_linking<P: AsRef<Path>>(out_dir: P) -> anyhow::Result<()> {
     assert!(compile_status.success());
 
     // Add meson output dir to rustc's library search path
-    println!("cargo:rustc-link-search={}", meson_build_dir.display());
+    println!("cargo::rustc-link-search={}", meson_build_dir.display());
 
     // Ask rustc to link rust binary to library produced by meson
-    println!("cargo:rustc-link-lib=samagon");
+    println!("cargo::rustc-link-lib=samagon");
 
     // Rerun this build script if meson build definition changes
-    println!("cargo:rerun-if-changed=meson.build");
+    println!("cargo::rerun-if-changed=meson.build");
 
     // Rerun this if anything inside src/ changes
-    println!("cargo:rerun-if-changed=src");
+    println!("cargo::rerun-if-changed=src");
 
     Ok(())
 }
